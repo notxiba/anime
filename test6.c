76a7e58dc01323ad491fe98132cda2ae8f0f2ffc
notxiba <yousseftalhane2007@gmail.com>
2025-09-02 23:54:19 +0100

    Add files via upload

---
+++ test6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    char x;
+    printf("entre un alphabi: ");
+    scanf("%C", &x);
+    if( x == a||x == 'e'||x == 'i'||x  == 'o'||x == 'u'||x == 'y'){
+        printf("%c la lphabi est un voyelle./n");
+    }
+    else{
+        printf("%c la lphabi est non voyelle./n ");
+
+    }
+    return 0;
+}
