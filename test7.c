62d5a9f9fc673e3db9b1f4cbec140c52e88ffcf2
notxiba <yousseftalhane2007@gmail.com>
2025-09-03 17:33:04 +0100

    first commit

--- test7.c
+++ test7.c
@@ -3,18 +3,18 @@ int main(){
     float Jours_accordes, Jours_utilises, Jours_restants;
     int Statut;
     printf("entre la number de jours_accordes: ");
-    scanf("%f", &jours accordes);
+    scanf("%f", &Jours_accordes);
     printf("entre la number de jours_utilises: ");
-    scanf("%f", &Jours_estants);
-    printf("entre lomplwa de temp: ")
+    scanf("%f", &Jours_restants);
+    printf("entre lomplwa de temp: ");
     scanf("%d", &Statut);
     if(Statut == 1){
-        Jours_restants = jours_accordes - jours_utilises
+        Jours_restants = Jours_accordes - Jours_utilises;
 
 
     }
     else if(Statut == 0){
-        Jours_restants = (Jours accordes / 2) - Jours utilises
+        Jours_restants = (Jours_accordes / 2) - Jours_utilises;
 
     }
     else{
