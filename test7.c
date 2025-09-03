f31bc5c2bc26d95e6259e19a1dfa2f82888f9436
notxiba <yousseftalhane2007@gmail.com>
2025-09-03 10:10:33 +0100

    test

---
+++ test7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    float Jours_accordes, Jours_utilises, Jours_restants;
+    int Statut;
+    printf("entre la number de jours_accordes: ");
+    scanf("%f", &jours accordes);
+    printf("entre la number de jours_utilises: ");
+    scanf("%f", &Jours_estants);
+    printf("entre lomplwa de temp: ")
+    scanf("%d", &Statut);
+    if(Statut == 1){
+        Jours_restants = jours_accordes - jours_utilises
+
+
+    }
+    else if(Statut == 0){
+        Jours_restants = (Jours accordes / 2) - Jours utilises
+
+    }
+    else{
+        printf("Vous avez dépassé le nombre de jours autorisés.");
+        
+    }
+    return 0;
+}
