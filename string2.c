36caf249b19b33f6fc47ec334159f24d0bc86a14
notxiba <yousseftalhane2007@gmail.com>
2025-09-08 14:56:01 +0100

    k

---
+++ string2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char name[50];
+    char last_name[50];
+    char lensombel = 0;
+    printf("entre le name: \n");
+    scanf("%s", name);
+    printf("entre last_nam: ");
+    scanf("%s", last_name);
+    printf("%s%s\n", name, last_name);
+
+}
