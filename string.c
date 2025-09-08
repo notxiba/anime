6b64eb41ea6ab5637a8c0a4b2ea2827b8c41eeb5
notxiba <yousseftalhane2007@gmail.com>
2025-09-08 12:02:52 +0100

    string

---
+++ string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX 50
+int main(){
+    char nom[MAX];
+    int longour=0;
+
+    printf("entre la nom: \n");
+    scanf("%s", nom);
+    for(int i=0; i<nom[i]; i++){
+        longour++;
+    }
+    printf("%d\n",longour);
+
+    
+
+}
